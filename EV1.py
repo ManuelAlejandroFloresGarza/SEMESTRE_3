d96ae23992670a9ecec8f20dab9df821d2723166
HectorMelendez619 <123921595+HectorMelendez619@users.noreply.github.com>
2023-09-04 03:05:38 -0600

    Update EV1.py

--- EV1.py
+++ EV1.py
@@ -128,3 +128,66 @@ class TallerMecanico:
                     print("Nota cancelada.")  # Imprime un mensaje de confirmación
         except ValueError:
             print("Error: Ingrese un número de folio válido.")  # Maneja errores de valor
+
+    def recuperar_nota(self):
+        try:
+            notas_canceladas = [nota for nota in self.notas if nota.cancelada]  # Filtra las notas que están canceladas
+            if not notas_canceladas:
+                print("No hay notas canceladas en el sistema.")  # Imprime un mensaje si no hay notas canceladas
+            else:
+                print("Notas canceladas:")
+                for nota in notas_canceladas:
+                    print(f"Folio: {nota.folio}, Fecha: {nota.fecha}, Cliente: {nota.cliente}")  # Imprime información de las notas canceladas
+                folio_recuperar = int(input("Ingrese el folio de la nota a recuperar (o 0 para cancelar): "))  # Lee el folio de la nota a recuperar
+                if folio_recuperar != 0:
+                    nota_recuperar = next((nota for nota in notas_canceladas if nota.folio == folio_recuperar), None)  # Busca la nota a recuperar por folio
+                    if nota_recuperar is None:
+                        print("La nota no está en el sistema.")  # Imprime un mensaje si la nota no se encuentra
+                    else:
+                        print(f"Folio: {nota_recuperar.folio}, Fecha: {nota_recuperar.fecha}, Cliente: {nota_recuperar.cliente}")  # Imprime información de la nota a recuperar
+                        confirmacion = input("¿Confirmar recuperación de la nota? (S/N): ")  # Pide confirmación para recuperar la nota
+                        if confirmacion.lower() == 's':
+                            nota_recuperar.cancelada = False  # Cambia el estado de la nota a no cancelada (recuperada)
+                            print("Nota recuperada.")  # Imprime un mensaje de confirmación
+        except ValueError:
+            print("Error: Ingrese un número de folio válido.")  # Maneja errores de valor
+
+    def ejecutar(self):
+     while True:
+        # Imprime el menú principal del programa
+        print("\nMenú Principal:")
+        print("1. Registrar una nota")
+        print("2. Consultar por período")
+        print("3. Cancelar una nota")
+        print("4. Recuperar una nota")
+        print("5. Salir")
+
+        # Solicita al usuario que seleccione una opción
+        opcion = input("Seleccione una opción (1/2/3/4/5): ")
+
+        if opcion == "1":
+            self.registrar_nota()  # Llama al método para registrar una nota si la opción es "1"
+        elif opcion == "2":
+            self.consultar_por_periodo()  # Llama al método para consultar por período si la opción es "2"
+        elif opcion == "3":
+            self.cancelar_nota()  # Llama al método para cancelar una nota si la opción es "3"
+        elif opcion == "4":
+            self.recuperar_nota()  # Llama al método para recuperar una nota si la opción es "4"
+        elif opcion == "5":
+            confirmacion = input("¿Está seguro de que desea salir del programa? (S/N): ")
+            if confirmacion.lower() == 's':
+                # Sale del bucle while (finaliza el programa) si se confirma la salida
+                print("Saliendo del programa. ¡Hasta luego!")
+                break
+            elif confirmacion.lower() == 'n':
+                continue  # Continúa en el bucle while si se elige no salir
+            else:
+                # Maneja una opción no válida si no se ingresa 'S' o 'N'
+                print("Opción no válida. Por favor, seleccione 'S' para salir o 'N' para continuar.")
+        else:
+            # Maneja una opción no válida si se ingresa un número diferente a 1, 2, 3, 4 o 5
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+if __name__ == "__main__":
+    taller = TallerMecanico()
+    taller.ejecutar()  # Inicia la ejecución del programa principal llamando al método "ejecutar" de la instancia
