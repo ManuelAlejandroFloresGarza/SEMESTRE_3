0123746466ae4add192c7af461feecfa27306843
ManuelAlejandroFloresGarza <143587001+ManuelAlejandroFloresGarza@users.noreply.github.com>
2023-11-06 11:03:58 -0600

    Update ev3.py

--- ev3.py
+++ ev3.py
@@ -320,3 +320,335 @@ def consulta_por_periodo():
             else:
                 print("No hay notas emitidas para el período especificado.")
             break
+def consulta_por_folio():
+    while True:
+        with sqlite3.connect("taller.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT n.folionota, n.fecha, c.nombrecliente, s.nombreservicio
+                FROM Notas n
+                JOIN Clientes c ON n.idcliente = c.idcliente
+                JOIN Servicios s ON n.idservicio = s.idservicio
+                WHERE n.status = 1
+                ORDER BY n.folionota
+            """)
+            notas = cursor.fetchall()
+
+            if not notas:
+                print("No hay notas activas en el sistema para consultar.")
+                return
+
+            print("Listado de notas activas:")
+            print("Folio  |  Fecha       |  Nombre del Cliente  |  Servicio")
+            for nota in notas:
+                print(f"{nota[0]:<7} | {nota[1]} | {nota[2]} | {nota[3]}")
+
+            try:
+                folio_consultar = int(input("Ingrese el folio de la nota que desea consultar o 0 para salir: "))
+            except ValueError:
+                print("Error: El folio debe ser un número válido.")
+                continue
+
+            if folio_consultar == 0:
+                print("Saliendo del menú de consulta por folio.")
+                break
+
+            if (folio_consultar,) in [(n[0],) for n in notas]:
+                cursor.execute("""
+                    SELECT n.folionota, n.fecha, c.nombrecliente, c.rfccliente, c.correocliente, s.nombreservicio, s.costoservicio
+                    FROM Notas n
+                    JOIN Clientes c ON n.idcliente = c.idcliente
+                    JOIN Servicios s ON n.idservicio = s.idservicio
+                    WHERE n.folionota = ? AND n.status = 1
+                """, (folio_consultar,))
+                nota = cursor.fetchone()
+
+                print("Detalle de la nota:")
+                print(f"Folio de la nota: {nota[0]}")
+                print(f"Fecha: {nota[1]}")
+                print(f"Nombre del Cliente: {nota[2]}")
+                print(f"RFC del Cliente: {nota[3]}")
+                print(f"Correo del Cliente: {nota[4]}")
+                print(f"Servicio: {nota[5]}")
+                print(f"Costo del servicio: {nota[6]:.2f}")
+            else:
+                print("El folio ingresado no corresponde a una nota activa en el sistema o no existe.")
+
+
+
+
+def exportar_reporte_csv(notas, fecha_inicial, fecha_final, promedio_monto):
+    if fecha_inicial is not None and fecha_final is not None:
+        filename = f"ReportePorPeriodo_{fecha_inicial.strftime('%m_%d_%Y')}_{fecha_final.strftime('%m_%d_%Y')}.csv"
+    else:
+        filename = "ReporteNotas.csv"
+
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Folio", "Fecha", "Nombre del Cliente"])
+        for nota in notas:
+            writer.writerow(nota)
+        if fecha_inicial is not None and fecha_final is not None:
+            writer.writerow(["Monto promedio del período:", "", promedio_monto])
+
+    print(f"El reporte ha sido exportado como {filename}.")
+
+def exportar_reporte_excel(notas, fecha_inicial, fecha_final, promedio_monto):
+    if fecha_inicial is not None and fecha_final is not None:
+        filename = f"ReportePorPeriodo_{fecha_inicial.strftime('%m_%d_%Y')}_{fecha_final.strftime('%m_%d_%Y')}.xlsx"
+    else:
+        filename = "ReporteNotas.xlsx"
+
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+    worksheet.title = "Reporte de Notas"
+
+    bold = openpyxl.styles.Font(bold=True)
+    bold_style = openpyxl.styles.NamedStyle(name="bold")
+    bold_style.font = bold
+
+    worksheet['A1'] = "Folio"
+    worksheet['B1'] = "Fecha"
+    worksheet['C1'] = "Nombre del Cliente"
+
+    worksheet['A1'].style = bold_style
+    worksheet['B1'].style = bold_style
+    worksheet['C1'].style = bold_style
+
+    row = 2
+
+    for nota in notas:
+        worksheet.cell(row=row, column=1, value=nota[0])
+        worksheet.cell(row=row, column=2, value=nota[1])
+        worksheet.cell(row=row, column=3, value=nota[2])
+        row += 1
+
+    if fecha_inicial is not None and fecha_final is not None:
+        worksheet.cell(row=row, column=1, value="Monto promedio del período:")
+        worksheet.cell(row=row, column=3, value=promedio_monto)
+        worksheet.cell(row=row, column=1).style = bold_style
+        worksheet.cell(row=row, column=3).style = bold_style
+
+    workbook.save(filename)
+
+    print(f"El reporte ha sido exportado como {filename}.")
+
+
+# Creamos la opción Clientes
+def Clientes():
+    while True:
+        mostrar_menu_clientes()
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            agregar_cliente()
+        elif opcion == "2":
+            listado_clientes()
+        elif opcion == "3":
+            print("Volviendo al menú principal.")
+            break
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+def agregar_cliente():
+    # Solicitar el nombre del servicio al usuario y validar que no esté en blanco
+    while True:
+        nombre = input("Ingrese el nombre completo del cliente: ")
+        if nombre.strip():  # Verifica que el nombre no esté en blanco
+            break
+        else:
+            print("El nombre no puede estar en blanco. Por favor, ingréselo nuevamente.") 
+
+    while True:
+        rfc = input("Ingrese el RFC del cliente: ")
+        if validar_rfc(rfc):
+            print("RFC válido.")
+            break  # Sale del bucle si el RFC es válido
+        else:
+            print("Error: el RFC no tiene un formato válido. Intente de nuevo.")
+
+    while True:
+        correo = input("Ingrese la dirección de correo electrónico: ")
+        if validar_correo(correo):
+            print("Correo electrónico válido.")
+            break # Sale del bucle si el correo es válido
+        else:
+            print("Error: la dirección de correo electrónico no tiene un formato válido.")
+
+    # Realizar la inserción en la base de datos
+    with sqlite3.connect("taller.db") as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("INSERT INTO Clientes (nombrecliente, rfccliente, correocliente) VALUES (?, ?, ?)", (nombre, rfc, correo))
+            conn.commit()
+            cursor.execute("SELECT idcliente FROM Clientes WHERE idcliente = (SELECT MAX(idcliente) FROM Clientes)")
+            clavecliente = cursor.fetchone()
+            print(f"Cliente agregado exitosamente con clave {clavecliente[0]}")
+        except Exception as e:
+            print(f"Error al agregar el servicio: {e}")
+
+
+def listado_clientes():
+    while True:
+        print("\nListado de clientes registrados:")
+        print("1. Ordenado por clave")
+        print("2. Ordenado por nombre")
+        print("3. Volver al menú anterior")
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":  # Listado de clientes ordenado por clave
+            ordenar_clientes_por_clave()
+        elif opcion == "2":  # Listado de clientes ordenado por nombre
+            ordenar_clientes_por_nombre()
+        elif opcion == "3":  # Volver al menú de consultas y reportes de clientes
+            print("Volviendo al menú de consultas y reportes de clientes.")
+            break
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+def ordenar_clientes_por_clave():
+    with sqlite3.connect("taller.db") as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT idcliente, nombrecliente, rfccliente, correocliente FROM Clientes ORDER BY idcliente")
+            clientes = cursor.fetchall()
+            if clientes:
+                print("\nListado de clientes activos ordenado por clave:")
+                for cliente in clientes:
+                    print(f"Clave: {cliente[0]}, Nombre: {cliente[1]}, RFC: {cliente[2]}, Correo: {cliente[3]}")
+
+                # Preguntar al usuario si desea exportar el reporte
+                while True:
+                    opcion_exportar = input("\n¿Desea exportar el reporte a CSV, Excel o regresar al menú de reportes? (CSV/Excel/Regresar): ").strip().lower()
+                    if opcion_exportar == "csv":
+                        exportar_reporte_clientes_csv(clientes, "ClientesActivosPorClave")
+                        break
+                    elif opcion_exportar == "excel":
+                        exportar_reporte_clientes_excel(clientes, "ClientesActivosPorClave")
+                        break
+                    elif opcion_exportar == "regresar":
+                        break
+                    else:
+                        print("Opción no válida. Por favor, seleccione una opción válida.")
+            else:
+                print("No hay clientes activos registrados.")
+        except sqlite3.Error as e:
+            print(f"Error al obtener el listado de clientes: {e}")
+
+def exportar_reporte_clientes_csv(data, report_name):
+    filename = f"{report_name}_{obtener_fecha_actual()}.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Clave", "Nombre", "RFC", "Correo"])
+        for cliente in data:
+            writer.writerow(cliente)
+    print(f"El reporte ha sido exportado como {filename}")
+
+def exportar_reporte_clientes_excel(data, report_name):
+    filename = f"{report_name}_{obtener_fecha_actual()}.xlsx"
+    workbook = openpyxl.Workbook()
+    worksheet = workbook.active
+    worksheet.title = report_name
+
+    bold = openpyxl.styles.Font(bold=True)
+    bold_style = openpyxl.styles.NamedStyle(name="bold")
+    bold_style.font = bold
+
+    worksheet['A1'] = "Clave"
+    worksheet['B1'] = "Nombre"
+    worksheet['C1'] = "RFC"
+    worksheet['D1'] = "Correo"
+
+    worksheet['A1'].style = bold_style
+    worksheet['B1'].style = bold_style
+    worksheet['C1'].style = bold_style
+    worksheet['D1'].style = bold_style
+
+    row = 2
+
+    for cliente in data:
+        worksheet.cell(row=row, column=1, value=cliente[0])
+        worksheet.cell(row=row, column=2, value=cliente[1])
+        worksheet.cell(row=row, column=3, value=cliente[2])
+        worksheet.cell(row=row, column=4, value=cliente[3])
+        row += 1
+
+    workbook.save(filename)
+
+    print(f"El reporte ha sido exportado como {filename}")
+
+def obtener_fecha_actual():
+    fecha_actual = datetime.datetime.now()
+    return fecha_actual.strftime("%m_%d_%Y")
+
+def ordenar_clientes_por_nombre():
+    with sqlite3.connect("taller.db") as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT idcliente, nombrecliente, rfccliente, correocliente FROM Clientes ORDER BY UPPER(nombrecliente)")
+            clientes = cursor.fetchall()
+            if clientes:
+                print("\nListado de clientes ordenado por nombre:")
+                for cliente in clientes:
+                    print(f"Clave: {cliente[0]}, Nombre: {cliente[1]}, RFC: {cliente[2]}, Correo: {cliente[3]}")
+
+                # Preguntar al usuario si desea exportar el reporte
+                while True:
+                    opcion_exportar = input("\n¿Desea exportar el reporte a CSV, Excel o regresar al menú de reportes? (CSV/Excel/Regresar): ").strip().lower()
+                    if opcion_exportar == "csv":
+                        exportar_reporte_clientes_csv(clientes, "ClientesPorNombre")
+                        break
+                    elif opcion_exportar == "excel":
+                        exportar_reporte_clientes_excel(clientes, "ClientesPorNombre")
+                        break
+                    elif opcion_exportar == "regresar":
+                        break
+                    else:
+                        print("Opción no válida. Por favor, seleccione una opción válida.")
+            else:
+                print("No hay clientes registrados.")
+        except sqlite3.Error as e:
+            print(f"Error al obtener el listado de clientes: {e}")
+
+
+
+
+# Creamos la opción Servicios
+def Servicios():
+    while True:
+        mostrar_menu_servicios()
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            agregar_servicio()
+        elif opcion == "2":
+            consultasreportes_servicios()
+        elif opcion == "3":
+            print("Volviendo al menú principal.")
+            break
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+def agregar_servicio():
+    # Solicitar el nombre del servicio al usuario y validar que no esté en blanco
+    while True:
+        nombre = input("Ingrese el nombre del servicio: ")
+        if nombre.strip():  # Verifica que el nombre no esté en blanco
+            break
+        else:
+            print("El nombre no puede estar en blanco. Por favor, ingréselo nuevamente.")
+    
+    try:
+        costo = float(input("Ingrese el costo del servicio: "))
+    except ValueError:
+        print("El costo debe ser un número válido.")
+        return
+    # Realizar la inserción en la base de datos
+    with sqlite3.connect("taller.db") as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute("INSERT INTO Servicios (nombreservicio, costoservicio) VALUES (?, ?)", (nombre, costo))
+            conn.commit()
+            cursor.execute("SELECT idservicio FROM Servicios WHERE idservicio = (SELECT MAX(idservicio) FROM Servicios)")
+            claveservicio = cursor.fetchone()
+            print(f"Servicio agregado exitosamente con clave {claveservicio[0]}")
+        except Exception as e:
+            print(f"Error al agregar el servicio: {e}")
